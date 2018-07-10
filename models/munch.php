b413fa6e63083d698e990ab89f1acb2498d0f2b4
chukwumaokere <chukwumaokere@yahoo.com>
2018-07-10 19:08:50 +0000

    introducing models

---
+++ models/munch.php
@@ -0,0 +1,29 @@
+<?php
+include 'config.php';
+global $root_web_dir;
+
+class munch {
+        //Public
+        public $something;
+
+        public function loadAsset($asset){
+                //file_get_contents?
+                //readfile?
+        }
+        public function loadPartial($partial){
+
+        }
+        public function loadView($view){
+                //synonymous with renderPage();
+        }
+        public function loadModel($model){
+
+        }
+        public function loadAction($action){
+
+        }
+        public function addToHead($html){
+
+        }
+
+}
